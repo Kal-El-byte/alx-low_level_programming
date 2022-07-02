cd4f87b84729a52a9e4c39337e92c9eba26bfbe9
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-01 20:31:10 -0700

    save

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,13 +7,13 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'z'; letter >= 'a'; letter--)
 		putchar(letter);
-	
 	putchar('\n');
 
 	return (0);
