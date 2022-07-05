0e064c06356b411820b347236b4f7f169c197af3
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-05 15:22:55 -0700

    correct

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,13 +7,14 @@ void print_alphabet_x10(void);
 int_islower(int c);
 int_isalpha(int c);
 int print_sign(int n);
-int_abs(int);
-int print_last_digit(int);
+int_abs(int r);
+int print_last_digit(int r);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void putformat(int);
 
 #endif /* MAIN.H */
 
