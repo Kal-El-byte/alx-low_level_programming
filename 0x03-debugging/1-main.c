ceb97393af225249acd1ba3207add9887b888bd8
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-06 16:03:19 -0700

    commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stidio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loops incoming :(\n")
+
+	i = 0;
+   /*
+   *	while (i < 10)
+   *	{
+   *	putchar(i);
+   *	}
+   */
+	printf("Infinite loop avoided! \\0/\n");
+
+	return (0);
+}
