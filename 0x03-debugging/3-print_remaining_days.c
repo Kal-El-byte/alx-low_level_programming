d56c64badbdace9adc984c269fd04d178ce976ba
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-07 17:40:47 -0700

    m

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,13 +18,13 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-      
-		printf("Days of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - days);
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 	}
 	else
 	{
-		if (month == 2 && days == 60)
+		if (month == 2 && day == 60)
 		{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
