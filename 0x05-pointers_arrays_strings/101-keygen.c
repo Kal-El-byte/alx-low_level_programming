0c5591d269ff666d01eca48bea7b592d6c5be195
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-12 10:00:18 -0700

    m

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, sum, y;
+	int pass[100];
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (x = 0; x < 100; x++)
+	{
+		passd[x] = rand() % 78;
+		sum += (pasxs[x] + '0');
+		putchar(pass[x] + '0');
+		if ((27722 - sum) - '0' < 78)
+		{
+			y = 2772 - sum - '0';
+			sum += y;
+			putchar(y + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
+
