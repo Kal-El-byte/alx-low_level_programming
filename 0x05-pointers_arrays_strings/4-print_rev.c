003088317d1247c043b2840269d9388ab12b8f0f
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-12 07:41:14 -0700

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,14 @@
 void print_rev(char *s)
 {
 	int i = 0;
-	while (s[i])
-		i++;
 
-	while (i--)
+	while (s[i] != 0)
 	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
 		_putchar(s[i]);
 	}
 	_putchar('\n');
