b180c083dde8c5b6b41ccac843f187254b4fcc08
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-12 09:32:00 -0700

    m

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,17 +8,17 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstname;
+	int sign = 1, resp = 0, firstNum;
 
-	for (firstname = 0; !(s[firstname] >= 48 && s[firstname] <= 57); firstname++)
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
-		if (s[firstname] == '-')
+		if (s[firstNum] == '-')
 		{
 			sign *= -1;
 		}
 	}
 
-	for (int i = firstname; s[i] >= 48 && s[i] <= 57; i++)
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
 		resp *= 10;
 		resp += (s[i] - 48);
