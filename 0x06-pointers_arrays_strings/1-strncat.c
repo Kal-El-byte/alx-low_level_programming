a8022da2671bf9d5fbd1738f533e3bbda59e0f67
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-14 10:53:22 -0700

    m

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 	;
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
 
-	/*should end with a end of string char*/
 	dest[dest_len + i] = '\0';
 
 	return (dest);
