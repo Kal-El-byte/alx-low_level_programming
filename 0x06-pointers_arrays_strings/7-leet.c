b0a6cc92896012bc55b1022737a3103f803b216c
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-14 12:36:15 -0700

    m

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,14 +10,14 @@ char *leet(char *s)
 {
 	int i, ii;
 	char s1[] = "aeotl";
-	char s1[] = "AEOTL";
+	char S1[] = "AEOTL";
 	char s2[] = "43071";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (ii = 0; ii < 5; ii++)
 		{
-			if (s[i] == s1[ii] || s[i] == s1[ii])
+			if (s[i] == s1[ii] || s[i] == S1[ii])
 			{
 				s[i] = s2[ii];
 				break;
