8102c1418c205bcacbcecd88068445f17cd28e8e
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-18 11:53:37 -0700

    m

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,20 +2,22 @@
 
 /**
  * print_chessboard - print the chessboard
- * @a: pointer to pieces to print
- *
+ * @a: board
  * Return: void
  */
+
+
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; i < 8; i++)
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
+		_putchar(a[i][j]);
 	}
+	_putchar('\n');
+}
+
 }
